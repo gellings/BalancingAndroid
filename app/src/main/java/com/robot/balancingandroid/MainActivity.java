ea2d0610f1649fdf9ca5556e0ef8e7e320ab82b5
Daniel Koch <daniel.koch@byu.edu>
2015-03-30 22:46:32 -0600

    Got optical flow (HW 5, task 1) running

--- app/src/main/java/com/robot/balancingandroid/MainActivity.java
+++ app/src/main/java/com/robot/balancingandroid/MainActivity.java
@@ -6,8 +6,17 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.video.Video;
 
 import android.app.Activity;
 import android.content.Context;
@@ -33,6 +42,15 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 
     private CameraBridgeViewBase openCvCameraView;
 
+    // optical flow stuff
+    Mat prevFrame;
+    static final int maxFeatures = 500;
+    static final double qualityLevel = 0.1;
+    static final double minDistance = 4.0;
+    boolean initialized;
+
+    static final Rect roi = new Rect(100, 100, 400, 400);
+
     PulseGenerator noise;
     Thread noiseThread;
 
@@ -44,6 +62,8 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        prevFrame = new Mat();
 
         estimator = new Estimator((SensorManager) this.getSystemService(Context.SENSOR_SERVICE));
 
@@ -84,6 +104,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         //estimator.registerListeners();
 
         openCvCameraView.enableView();
+        initialized = false;
     }
 
     @Override
@@ -142,11 +163,46 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     public Mat onCameraFrame(CvCameraViewFrame inputFrame)
     {
         Mat frame = inputFrame.gray();
-        Mat edges = new Mat();
-        Imgproc.Canny(frame, edges, 10, 100);
+
+        if (!initialized)
+        {
+            frame.copyTo(prevFrame);
+            initialized = true;
+        }
 
         Mat display = new Mat();
-        Imgproc.cvtColor(edges, display, Imgproc.COLOR_GRAY2BGR);
+        Imgproc.cvtColor(prevFrame, display, Imgproc.COLOR_GRAY2BGR);
+
+        MatOfPoint features = new MatOfPoint();
+        Imgproc.goodFeaturesToTrack(prevFrame, features, maxFeatures, qualityLevel, minDistance);
+        Point featuresArray[] = features.toArray();
+
+        if (featuresArray.length > 0) {
+
+            MatOfPoint2f prevPts = new MatOfPoint2f();
+            prevPts.fromArray(featuresArray);
+
+            MatOfPoint2f nextPts = new MatOfPoint2f();
+            MatOfByte status = new MatOfByte();
+            MatOfFloat err = new MatOfFloat();
+
+            Video.calcOpticalFlowPyrLK(prevFrame, frame, prevPts, nextPts, status, err);
+
+            Point prevPtsArray[] = prevPts.toArray();
+            Point nextPtsArray[] = nextPts.toArray();
+            byte statusArray[] = status.toArray();
+
+            for (int i = 0; i < prevPtsArray.length; i++) {
+                if (statusArray[i] != 0) {
+                    Core.circle(display, prevPtsArray[i], 5, new Scalar(0, 255, 0));
+                    Core.line(display, prevPtsArray[i], nextPtsArray[i], new Scalar(0, 0, 255));
+                }
+            }
+        } else {
+            Log.e(TAG, "No points found!");
+        }
+
+        frame.copyTo(prevFrame);
         return display;
     }
 }
